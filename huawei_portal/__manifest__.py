41fa591a25d45ca5fab8da5c4a0300d247d9c244
Rona Setyana <rona@jukesolutions.com>
2020-07-15 11:31:18 +0700

    Embed

--- huawei_portal/__manifest__.py
+++ huawei_portal/__manifest__.py
@@ -24,6 +24,7 @@
     # always loaded
     'data': [
         'security/ir.model.access.csv',
+        'views/dashboard.xml',
         'views/views.xml',
         'views/templates.xml',
     ],
---
+++ huawei_portal/models/embed.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api, _
+
+class Embed(models.Model):
+    _name = 'huawei_portal.embed'
+    _description = 'Tableau'
+
